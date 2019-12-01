0a1c2655aae1ceda6b0888e228e8087c7e9a74f0
Luke Hansford <mail@lukehansford.me>
2019-12-01 12:55:56 +0100

    Day 1 - Part 1

---
+++ 2019/1/day1.ex
@@ -0,0 +1,11 @@
+module_masses = String.split(File.read!("./input.txt"), "\n")
+
+part1_result = module_masses
+  |> Enum.map(fn x -> Integer.parse(x) end)
+  |> Enum.filter(fn x -> x != :error end)
+  |> Enum.map(fn x -> elem(x, 0) end)
+  |> Enum.map(fn x -> Integer.floor_div(x, 3) - 2 end)
+  |> Enum.reduce(0, fn(x, acc) -> x + acc end)
+
+IO.puts(result)
+
